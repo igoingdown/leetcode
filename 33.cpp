be612df28e2c3e5908ef8108ed0050a9a5bf6bb2
igoingdown <fycjmingxing@126.com>
2017-02-27 19:45:39 +0800

    commit time: 2017-02-27 19:45:39

---
+++ 33.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.size() == 0) {
+            return -1;
+        }
+        int low = 0, high = nums.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > nums[low]) {
+                if (nums[mid] > target && nums[low] <= target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (nums[mid] < nums[low]) {
+                if (nums[low] <= target) {
+                    high = mid - 1;
+                } else {
+                    low++;
+                }
+            } else {
+                low++;
+            }
+        }
+        return nums[low] == target ? low : -1;
+    }
+};
