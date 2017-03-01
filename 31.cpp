53c485c179dad23d62ec6aff11751cc261b74512
igoingdown <fycjmingxing@126.com>
2017-03-01 09:56:20 +0800

    commit time: 2017-03-01 09:56:20

---
+++ 31.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.size() <= 1) {
+            return;
+        }
+        int i = nums.size() - 2;
+        for (; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                break;
+            }
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+        if (i == -1) {
+            return;
+        }
+        auto iptr = upper_bound(nums.begin() + i + 1, nums.end(), nums[i]);
+        swap(nums[i], *iptr);
+    }
+};
