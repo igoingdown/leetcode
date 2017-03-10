3376d9447ccb50c8d6d1ec85cb6c60dca65a112d
igoingdown <fycjmingxing@126.com>
2017-03-10 11:08:11 +0800

    commit time: 2017-03-10 11:08:11

--- 16.cpp
+++ 16.cpp
@@ -2,32 +2,28 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int n = nums.size(), res = 0;
-        if (n <= 3) {
-            for (auto n: nums) {
-                res += n;
-            }
-            return res;
-        }
-        if (nums[0] + nums[1] + nums[2] >= target) {
-            return nums[0] + nums[1] + nums[2];
-        }
-        if (nums[n - 1] + nums[n - 2] + nums[n - 3] <= target) {
-            return nums[n - 1] + nums[n - 2] + nums[n - 3];
-        }
-        res = nums[0] + nums[1] + nums[2];
+        int res = nums[0] + nums[1] + nums[2];
+        int n = nums.size();
         for (int i = 0; i < n - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int left = i + 1, right = n - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                abs(res - target) < abs(sum - target) ? res : res = sum;
-                if (res == target) {
+            if (nums[i] + nums[i + 1] + nums[i + 2] > target) {
+                int sum = nums[i] + nums[i + 1] + nums[i + 2];
+                abs(res - target) > abs(sum - target) ? res = sum : res;
+                return res;
+            }
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                abs(res - target) > abs(sum - target) ? res = sum : res;
+                if (sum < target) {
+                    l++;
+                } else if (sum > target) {
+                    r--;
+                } else {
                     return res;
                 }
-                sum < target ? left++ : right--;
             }
         }
         return res;
