06f817d4d8f83a1e1072f340b6ba6932de92b3f8
igoingdown <fycjmingxing@126.com>
2017-04-16 23:06:01 +0800

    commit time: 2017-04-16 23:06:01

---
+++ 365.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+public:
+    bool canMeasureWater(int x, int y, int z) {
+        if (z == 0) {
+            return true;
+        }
+        if (x + y < z) {
+            return false;
+        }
+        return z % gcd(x, y) == 0;
+    }
+};
