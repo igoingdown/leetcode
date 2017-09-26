6f8486c96bfb2cdb5af4ec4b78814614802c40e7
igoingdown <fycjmingxing@126.com>
2017-09-27 00:29:50 +0800

    and commit time: 2017-09-27 00:29:50.

---
+++ 139.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && find(wordDict.begin(), wordDict.end(), s.substr(j, i - j)) != wordDict.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
