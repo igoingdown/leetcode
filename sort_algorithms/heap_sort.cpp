2c90b16829b729bda059f883d1e750ea6e18d800
igoingdown <fycjmingxing@126.com>
2017-10-19 00:17:00 +0800

    and commit time: 2017-10-19 00:17:00.

--- sort_algorithms/heap_sort.cpp
+++ sort_algorithms/heap_sort.cpp
@@ -9,11 +9,11 @@ void max_heapfy(vector<int>& s, int beg, int end);
 int main(int argc, char const *argv[])
 {
 	/* code */
-	int a[5] = {3, 6, 2, 9, 0};
-	vector<int> b(a, a + 5);
+	int a[7] = {8, 5, 4, 6, 7,3,9};
+	vector<int> b(a, a + 7);
 	heap_sort(b);
-	for (int i = 0; i < 5; i++) {
-		cout << b[i] << endl;
+	for (int n: b) {
+		cout << n << endl;
 	}
 	return 0;
 }
@@ -22,9 +22,9 @@ int main(int argc, char const *argv[])
 void heap_sort(vector<int>& s) {
 	int n = s.size();
 	for (int i = n / 2 - 1; i >= 0; i--) {
-		max_heapfy(s, i, n - 1);
+		max_heapfy(s, i, n);
 	}
-	for (int i = n - 1, i > 0; i--) {
+	for (int i = n - 1; i > 0; i--) {
 		swap(s[i], s[0]);
 		max_heapfy(s, 0, i);
 	}
@@ -33,8 +33,8 @@ void heap_sort(vector<int>& s) {
 
 void max_heapfy(vector<int>& s, int b, int e) {
 	int cur = b, child = 2 * cur + 1;
-	while (child <= e) {
-		if (child + 1 <= e && s[child] < s[child + 1]) {
+	while (child < e) {
+		if (child + 1 < e && s[child] < s[child + 1]) {
 			child++;
 		}
 		if (s[cur] < s[child]) {
