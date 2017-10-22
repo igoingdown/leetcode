9f45bb703837ca81ffa9611cd92823b054e7c5d9
igoingdown <fycjmingxing@126.com>
2017-10-23 00:40:35 +0800

    and commit time: 2017-10-23 00:40:35.

--- 100.cpp
+++ 100.cpp
@@ -10,12 +10,12 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (! p && ! q) {
-            return true;
-        } else if (p && q && p->val == q->val) {
-            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-        } else {
+        if (p && q) {
+            return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        } else if (p || q) {
             return false;
+        } else {
+            return true;
         }
     }
 };
