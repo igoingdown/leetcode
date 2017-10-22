4ae0050fdd2d1da233ae300f0014ebc29af71b78
igoingdown <fycjmingxing@126.com>
2017-10-23 02:31:36 +0800

    and commit time: 2017-10-23 02:31:36.

---
+++ 98_no_recursion.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> s;
+        TreeNode * pre = NULL;
+        while (root || !s.empty()) {
+            while (root) {
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top();
+            s.pop();
+            if (pre && pre->val >= root->val) {
+                return false;
+            }
+            pre = root;
+            root = root->right;
+        }
+        return true;
+    }
+};
