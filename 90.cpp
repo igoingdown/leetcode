501ef93bdc188a0fe29e75dd9d4c5e3d936d534c
igoingdown <fycjmingxing@126.com>
2017-10-25 14:27:12 +0800

    and commit time: 2017-10-25 14:27:12.

--- 90.cpp
+++ 90.cpp
@@ -1,22 +1,23 @@
 class Solution {
+private:
+    void dfs(vector<vector<int>>& res, vector<int>& tmp, vector<int>& nums, int start) {
+        if (start == nums.size()) return;
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            tmp.push_back(nums[i]);
+            res.push_back(tmp);
+            dfs(res, tmp, nums, i + 1);
+            tmp.pop_back();
+        }
+    }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res(1, vector<int> (0));
+        int s = nums.size();
+        if (!s) return res;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
         vector<int> tmp;
-        backtrack(res, tmp, nums, 0);
+        dfs(res, tmp, nums, 0);
         return res;
-    }
-    void backtrack(vector<vector<int>>& res, vector<int>& tmp, vector<int>& nums, int s) {
-        res.push_back(tmp);
-        for (int i = s; i < nums.size(); i++) {
-            if (i > s && nums[i] == nums[i - 1]) {
-                continue;
-            } else {
-                tmp.push_back(nums[i]);
-                backtrack(res, tmp, nums, i + 1);
-                tmp.pop_back();
-            }
-        }
     }
 };
