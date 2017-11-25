54eb12bf32c0284b56db510a94299d44ace69a9f
igoingdown <fycjmingxing@126.com>
2017-11-25 10:34:09 +0800

    kill hashmap  and commit time: 2017-11-25 10:34:09.

--- 49.cpp
+++ 49.cpp
@@ -1,67 +1,16 @@
-//
-//  main.cpp
-//  49
-//
-//  Created by 赵明星 on 2017/3/29.
-//  Copyright © 2017年 赵明星. All rights reserved.
-//
-
-#include <iostream>
-#include <vector>
-#include <string>
-#include <map>
-
-using namespace std;
-
-vector<vector<string>> groupAnagrams(vector<string>& strs);
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    vector<string> strs{"c", "c", "c", "c"};
-    vector<vector<string>> res = groupAnagrams(strs);
-    for (auto v: res) {
-        for (auto s: v) {
-            cout << s;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<vector<int>, vector<string>> m;
+        for (string s : strs) {
+            vector<int> v(26, 0);
+            for (char c: s) v[c - 'a']++;
+            m[v].push_back(s);
         }
-        cout << endl;
-    }
-    std::cout << "Hello, World!\n";
-    return 0;
-}
-
-vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    map<vector<int>, vector<string>> m;
-    for (auto s: strs) {
-        vector<int> temp(26, 0);
-        for (auto c: s) {
-            temp[c - 'a']++;
+        vector<vector<string>> res;
+        for (auto iter = m.begin(); iter != m.end(); iter++) {
+            res.push_back(iter->second);
         }
-        m[temp].push_back(s);
+        return res;
     }
-    vector<vector<string>> res;
-    for (auto i = m.begin(); i != m.end(); i++) {
-        res.push_back(i->second);
-    }
-    return res;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
