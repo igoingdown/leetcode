b2ed76329ca6f4fe6c54bd29c8a0e00680e0c711
igoingdown <fycjmingxing@126.com>
2017-12-20 01:21:12 +0800

    kill tree  and commit time: 2017-12-20 01:21:12.

---
+++ 116_more_efficient.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        TreeLinkNode* levelStart = root;
+        while (levelStart) {
+            TreeLinkNode* cur = levelStart;
+            while (cur) {
+                if (cur->left) cur->left->next = cur->right;
+                if (cur->right && cur->next) cur->right->next = cur->next->left;
+                cur = cur->next;
+            }
+            levelStart = levelStart->left;
+        }
+    }
+};
