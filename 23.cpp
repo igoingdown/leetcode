4c0367e8f3db950834fe14862e18a1a693505161
igoingdown <fycjmingxing@126.com>
2018-01-10 01:25:40 +0800

    kill heap  and commit time: 2018-01-10 01:25:40.

--- 23.cpp
+++ 23.cpp
@@ -1,54 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-struct ListNode
-{
-	int val;
-	ListNode* next;
-	ListNode(int x): val(x), next(NULL) {}
-};
-
-class Comparison
-{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Comparison{
 public:
-	bool operator() (ListNode* a, ListNode* b) {
-		return a->val > b->val;
-	}
-};
-
-ListNode* mergeKLists(vector<ListNode*>& lists);
-
-int main(int argc, char const *argv[])
-{
-	ListNode* a = new ListNode(1);
-	ListNode* b = new ListNode(0);
-	vector<ListNode*> v(2, a);
-	v[1] = b;
-	auto head = mergeKLists(v);
-	while (head) {
-		cout << head->val << endl;
-		head = head->next;
-	}
-	return 0;
-}
-
-ListNode* mergeKLists(vector<ListNode*>& lists) {
-    priority_queue<ListNode*, vector<ListNode*>, Comparison> q(lists.begin(), lists.end());
-    ListNode* head = new ListNode(0), *last = head;
-    while (!q.empty()) {
-        auto cur = q.top();
-        q.pop();
-        if (cur->next) q.push(cur->next);
-        ListNode* tmp = last->next;
-        last->next= cur;
-        cur->next = tmp;
-        last = cur;
+    bool operator() (ListNode* a, ListNode* b) {
+        return a->val > b->val;
     }
-    return head->next;
-}
-
-
-
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Comparison> q;
+        for (auto n : lists) {
+            if (n) q.push(n);
+        }
+        ListNode* head = new ListNode(0), *tmp = head;
+        while (!q.empty()) {
+            auto cur = q.top();
+            q.pop();
+            tmp->next = cur;
+            tmp = cur;
+            if (cur->next) q.push(cur->next);
+        }
+        tmp->next = NULL;
+        return head->next;
+    }
+};
