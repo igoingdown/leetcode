d71913a6b5e51292d1b127d4e9475bf3301d2067
igoingdown <fycjmingxing@126.com>
2018-01-10 11:06:27 +0800

    kill sort  and commit time: 2018-01-10 11:06:27.

--- sort_algorithms/merge_sort.cpp
+++ sort_algorithms/merge_sort.cpp
@@ -2,48 +2,34 @@
 #include <vector>
 
 using namespace std;
-
-void mergeArray(vector<int>& s, int l, int m, int r);
-void mergeSort(vector<int>& s, int l, int r);
+void merge_sort(vector<int>& v, int b, int e);
+void merge(vector<int>& v, int b, int mid, int e);
 
 int main(void) {
-	int a[7] = {10, 3, 13, 4, 2, 1, 37};
-	vector<int> v(a, a + 7);
-	mergeSort(v, 0, 7);
-	for (int n : v) {
-		cout << n << " ";
-	}
-	cout << endl;
+	int a[5] = {1, 10, 4, 2, 0};
+	vector<int> v(a, a + 5);
+	merge_sort(v, 0, 4);
+	for (int n : v) cout << n << endl;
 	return 0;
 }
 
-void mergeArray(vector<int>& s, int l, int m, int r) {
-	int tb = 0, lb = l, rb = m;
-	vector<int> tmp(r - l, 0);
-	while (lb < m && rb < r) {
-		if (s[lb] < s[rb]) {
-			tmp[tb++] = s[lb++];
-		} else {
-			tmp[tb++] = s[rb++];
-		}
-	}
-	while(lb < m) {
-		tmp[tb++] = s[lb++];
-	}
-	while (rb < r) {
-		tmp[tb++] = s[rb++];
-	}
-	for (int i = 0; i < tb; i++) {
-		s[l + i] = tmp[i];
+void merge_sort(vector<int>& v, int b, int e) {
+	if (b < e) {
+		int mid = b + (e - b) / 2;
+		merge_sort(v, b, mid);
+		merge_sort(v, mid + 1, e);
+		merge(v, b, mid, e);
 	}
 }
 
-
-void mergeSort(vector<int>& s, int l, int r) {
-	int mid = l + ((r - l) >> 1);
-	if (mid > l) {
-		mergeSort(s, l, mid);
-		mergeSort(s, mid, r);
-		mergeArray(s, l, mid, r);
+void merge(vector<int>& v, int b, int mid, int e) {
+	vector<int> tmp;
+	int lb = b, le = mid, rb = mid + 1, re = e;
+	while (lb <= le && rb <= re) {
+		if (v[lb] < v[rb]) tmp.push_back(v[lb++]);
+		else tmp.push_back(v[rb++]);
 	}
+	while (lb <= le) tmp.push_back(v[lb++]);
+	while (rb <= re) tmp.push_back(v[rb++]);
+	for (int i = 0; i < tmp.size(); i++) v[i + b] = tmp[i];
 }
--- sort_algorithms/quick_sort.cpp
+++ sort_algorithms/quick_sort.cpp
@@ -9,15 +9,10 @@ int partition_2(vector<int>& s, int l, int r);
 void quick_sort_with_inline_partition(vector<int> & nums, int l, int r);
 
 int main(void) {
-	vector<int> v;
-	v.push_back(3);
-	v.push_back(2);
-	v.push_back(9);
-	v.push_back(5);
+	int a[4] = {3, 2, 9, 5};
+	vector<int> v(a, a + 4);
 	quick_s(v, 0, 3);
-	for (auto i : v) {
-		cout << i << endl;
-	}
+	for (auto i : v) cout << i << endl;
 	return 0;
 }
 
@@ -34,18 +29,10 @@ void quick_s(vector<int>& s, int l, int r) {
 int partition_1(vector<int>& s, int l, int r) {
 	int i = l, j = r, x = s[l];
 	while (i < j) {
-		while (i < j && s[j] >= x) {
-			j--;
-		}
-		if (i < j) {
-			s[i++] = s[j];
-		}
-		while (i < j && s[i] <= x) {
-			i++;
-		}
-		if (i < j) {
-			s[j--] = s[i];
-		}
+		while (i < j && s[j] >= x) j--;
+		if (i < j) s[i++] = s[j];
+		while (i < j && s[i] <= x) i++;
+		if (i < j) s[j--] = s[i];
 	}
 	s[i] = x;
 	return i;
@@ -55,15 +42,9 @@ int partition_1(vector<int>& s, int l, int r) {
 int partition_2(vector<int>& s, int l, int r) {
 	int i = l + 1, j = r, x = s[l];
 	while (i <= j) {
-		if (s[i] > x && s[j] < x) {
-			swap(s[i++], s[j--]);
-		} 
-		if (s[i] <= x) {
-			i++;
-		} 
-		if (s[j] >= x) {
-			j--;
-		} 
+		if (s[i] > x && s[j] < x) swap(s[i++], s[j--]);
+		if (s[i] <= x) i++; 
+		if (s[j] >= x) j--;
 	}
 	swap(s[l], s[j]);
 	return j;
@@ -75,18 +56,10 @@ void quick_sort_with_inline_partition(vector<int> & nums, int l, int r) {
     if (l < r) {
         int i = l, j = r, x = nums[l];
         while (i < j) {
-            while (i < j && nums[j] >= x) {
-                j--;
-            }
-            if (i < j) {
-                nums[i++] = nums[j];
-            }
-            while (i < j && nums[i] <= x) {
-                i++;
-            }
-            if (i < j) {
-                nums[j--] = nums[i];
-            }
+            while (i < j && nums[j] >= x) j--;
+            if (i < j) nums[i++] = nums[j];
+            while (i < j && nums[i] <= x) i++;
+            if (i < j) nums[j--] = nums[i];
         }
         nums[i] = x;
         quick_sort(nums, l, i - 1);
