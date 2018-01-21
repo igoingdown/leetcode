3407c980c2c526fc758bde96528585a430f6ccd5
igoingdown <fycjmingxing@126.com>
2018-01-21 23:15:07 +0800

    kill frequent  and commit time: 2018-01-21 23:15:07.

--- 39.cpp
+++ 39.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
         vector<vector<int>> res;
         vector<int> path;
-        dfs(res, candidates, path, 0, target);
+        dfs(res, path, candidates, target, 0);
         return res;
     }
-    
-    void dfs(vector<vector<int>>& res, vector<int>& cand, vector<int>& path, int cur, int target) {
+    void dfs(vector<vector<int>>& res, vector<int>& path, vector<int>& can, int target, int start) {
+        if (start >= can.size() || target < 0) return;
         if (target == 0) {
             res.push_back(path);
             return;
         }
-        for (int i = cur; i < cand.size() && cand[i] <= target; i++) {
-            path.push_back(cand[i]);
-            dfs(res, cand, path, i, target - cand[i]);
+        for (int i = start; i < can.size(); i++) {
+            path.push_back(can[i]);
+            dfs(res, path, can, target - can[i], i);
             path.pop_back();
         }
     }
--- 43.cpp
+++ 43.cpp
@@ -17,28 +17,18 @@ int main(int argc, char const *argv[]) {
 }
 
 string multiply(string num1, string num2) {
-    int s1 = num1.size(), s2 = num2.size();
-    int maxLen = s1 + s2;
-    int c = 0;
-    string p(maxLen, '0');
-    for (int i = 0; i < maxLen; i++) {
-        int res = c;
+    int s1 = num1.size(), s2 = num2.size(), max_len = s1 + s2, c = 0;
+    if (max_len == 0) return "";
+    string res(max_len, '0');
+    for (int i = 0; i < max_len; i++) {
+        int bit_res = c;
         for (int j = 0; j <= i; j++) {
-            if (j < s1 && i - j < s2) {
-                res += (num1[s1 - 1 - j] - '0') * (num2[s2 - 1 - i + j] - '0');
-            }
+            if (j < s1 && i - j < s2) bit_res += (num1[s1 - 1 - j] - '0') * (num2[s2 - 1 - i + j] - '0');
         }
-        c = res / 10;
-        res %= 10;
-        p[maxLen - 1 - i] = char(res + '0');
+        c = bit_res / 10;
+        res[max_len - 1 - i] = char(bit_res % 10 + '0');
     }
     int pos = 0;
-    while (p[pos] == '0') {
-        pos++;
-    }
-    p = p.substr(pos);
-    if (p.size() == 0) {
-        p += "0";
-    }
-    return p;
+    while (res[pos] == '0') pos++;
+    return pos == res.size() ? "0" : res.substr(pos);
 }
