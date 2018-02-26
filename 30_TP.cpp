770f90d18c5837556ad13d2caeebbdec483d8465
igoingdown <fycjmingxing@126.com>
2018-02-26 21:17:35 +0800

    kill frequent  and commit time: 2018-02-26 21:17:35.

--- 30_TP.cpp
+++ 30_TP.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> m, copy;
-        for (string word : words) m[word]++;
         vector<int> res;
-        if (words.size() == 0) return res;
-        int l = words[0].size(), ss = s.size(), ws = words.size();
-        for (int i = 0; i <= ss - l * ws; i++) {
-            unordered_map<string, int> copy(m);
-            for (int j = 0; j < words.size(); j++) {
-                auto str = s.substr(i + l * j, l);
+        int ws = words.size(), ss = s.size(), l;
+        if (ws == 0) return res;
+        l = words[0].size();
+        map<string, int> m;
+        for (auto word : words) m[word]++;
+        for (int i = 0; i <= ss - ws * l; i++) {
+            map<string, int> copy(m);
+            for (int j = 0; j < ws; j++) {
+                string str = s.substr(i + l * j, l);
                 if (copy.find(str) != copy.end()) {
                     if (copy[str] == 1) copy.erase(str);
                     else copy[str]--;
