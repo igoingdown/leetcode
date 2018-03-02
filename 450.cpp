60732466ee1d963dcc5e8462cedad77244e0d7f6
igoingdown <fycjmingxing@126.com>
2018-03-02 09:51:12 +0800

    kill frequent  and commit time: 2018-03-02 09:51:12.

--- 450.cpp
+++ 450.cpp
@@ -8,41 +8,31 @@
  * };
  */
 class Solution {
-private:
-    int findAndDelete(TreeNode* node) {
-        int res;
-        if (!node->left->right) {
-            res = node->left->val;
-            node->left = node->left->left;
-            return res;
-        }
-        auto p = node;
-        node = node->left;
-        while (node->right) {
-            p = node;
-            node = node->right;
-        }
-        res = node->val;
-        p->right = node->left;
-        return res;
-    }
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if (!root) return root;
         if (root->val == key) {
-            if (root->left && root->right) {
-                int val = findAndDelete(root);
-                root->val = val;
-            } else if (!root->left && !root->right) return NULL;
+            if (root->left && root->right) root->val = findAndDelete(root);
+            else if (!root->left && !root->right) root = NULL;
             else {
-                TreeNode* tmp;
-                tmp = root->left ? root->left : root->right;
-                root->val = tmp->val;
-                root->left = tmp->left;
-                root->right = tmp->right;
+                TreeNode *tmp = root->left ? root->left : root->right;
+                root->left = tmp->left; root->val = tmp->val; root->right = tmp->right;
             }
         } else if (root->val < key) root->right = deleteNode(root->right, key);
         else root->left = deleteNode(root->left, key);
         return root;
+    }
+    
+    int findAndDelete(TreeNode *root) {
+        TreeNode *parent = root->left, *child = parent->right;
+        if (!child) {
+            int res = parent->val;
+            root->left = parent->left; return res;
+        }
+        while (child->right) {
+            parent = child; child = child->right;
+        }
+        parent->right = child->left; 
+        return child->val;
     }
 };
