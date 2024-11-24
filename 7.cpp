61e7d7e8943ed333045a44d6374836195ca9f5dc
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2024-11-24 17:36:49 +0800

    [fix]更新解法
    Change-Id: I360d34a1b7cdfddfa12dd15d728ec0921688337a

--- 7.cpp
+++ 7.cpp
@@ -1,35 +1,9 @@
 class Solution {
 public:
-    int reverse(int x) {
-        int sign = 1, res = 0;
-        if (x == INT_MIN) return res;
-        if (x < 0) {
-            sign = -1;
-            x *= -1;
-        }
-        while (x) {
-            res = res * 10 + x % 10;
-            x /= 10;
-            if ((res > INT_MAX / 10 && x) || (res == INT_MAX / 10) && ((x > INT_MAX % 10 && sign > 0) || (x > INT_MAX % 10 + 1 && sign < 0))) return 0;
-        }
-        return res * sign;
-    }
-
-
-    int reverse2(int x) {
-        long long a = x;
-        int sign = 1;
-        if (a < 0) {
-            a = -a;
-            sign = -1;
-        }
-        long long res = 0;
-        while (a) {
-            res = res * 10 + a % 10;
-            a /= 10;
-        }
-        res *= sign;
-        return res > INT_MAX || res < INT_MIN ? 0 : res;
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for (int i = 0; i < 32; i++) res = (res << 1) | (n >> i & 1);
+        return res;
     }
 };
 
