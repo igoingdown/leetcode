c4fcfd850f6debc31d72c70fea9f763a82ac93e5
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2025-03-30 16:36:14 +0800

    [fix]使用新的写法
    Change-Id: Ifb6b8802ef0043ffe61778d6dd1ea898f4fba19b

--- 799_bfs.cpp
+++ 799_bfs.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<double> cur;
-        cur.push_back(double(poured));
-        for (int i = 0; i<query_row; i++) {
-            vector<double> next(cur.size() + 1, 0);
-            for (int j = 0; j < cur.size(); j++) {
-                if (cur[j] > 1) {
-                    next[j] += ((cur[j] - 1) / 2);
-                    next[j+1] += ((cur[j] - 1) / 2);
-                }
+        vector<double> cur(1, poured);
+        while(query_row--) {
+            vector<double> next(cur.size() + 1);
+            for(int i = 0; i < cur.size(); i++) {
+                double overflow = max(cur[i] - 1.0, 0.0) / 2.0;
+                next[i] += overflow;
+                next[i + 1] += overflow;
             }
             swap(cur, next);
         }
-        if (cur[query_glass] > 1) {
-            cur[query_glass] = 1;
-        }
-        return cur[query_glass];
+        return cur[query_glass] > 1 ? 1 : cur[query_glass];
     }
 };
