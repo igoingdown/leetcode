25591a37b531e4ef1a1a9e338c4ced22df63c2cd
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2025-09-13 14:10:29 +0800

    [fix]更新排序代码
    Change-Id: Idcc0ab92dd4407583313c9a4ec2a16c31699e6aa

--- solutions/215_priority_queue.cpp
+++ solutions/215_priority_queue.cpp
@@ -1,16 +1,39 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> q;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i < k) q.push(nums[i]);
-            else {
-                if (nums[i] > q.top()) {
-                    q.pop();
-                    q.push(nums[i]);
-                }
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+int findKthLargestNum(vector<int> &nums, int k) {
+    priority_queue<int,vector<int>, greater<int>> q;
+    for (int n : nums) {
+        if (q.size() < k) {
+            q.push(n);
+        } else {
+            if (q.top() <= n) {
+                q.push(n);
+                q.pop();
             }
         }
-        return q.top();
     }
-};
+    return q.top();
+}
+
+int main() {
+    // Test example 1
+    vector<int> nums1 = {3, 2, 1, 5, 6, 4};
+    int k1 = 2;
+    cout << "Test 1: findKthLargestNum({3, 2, 1, 5, 6, 4}, 2) = " << findKthLargestNum(nums1, k1) << endl;
+    
+    // Test example 2
+    vector<int> nums2 = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+    int k2 = 4;
+    cout << "Test 2: findKthLargestNum({3, 2, 3, 1, 2, 4, 5, 5, 6}, 4) = " << findKthLargestNum(nums2, k2) << endl;
+    
+    // Test example 3 - edge case
+    vector<int> nums3 = {1};
+    int k3 = 1;
+    cout << "Test 3: findKthLargestNum({1}, 1) = " << findKthLargestNum(nums3, k3) << endl;
+    
+    return 0;
+}
