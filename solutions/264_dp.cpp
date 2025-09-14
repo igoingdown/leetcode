478a9be0040fd0f6f06b5d5c5117dcfe0bcf1511
ZHAOMINGXING.93 <ZHAOMINGXING.93@bytedance.com>
2025-09-14 23:18:49 +0800

    [fix]更新 dp 解法

--- solutions/264_dp.cpp
+++ solutions/264_dp.cpp
@@ -1,23 +1,30 @@
-class Solution {
-public:
-    int nthUglyNumber(int n) {
-        vector<int> dp(n+1);
-        dp[1] = 1;
-        int p2=1, p3=1,p5=1;
-        for (int i = 2; i<= n; i++) {
-            int t2 = dp[p2] * 2, t3 = dp[p3] * 3, t5 = dp[p5] * 5;
-            int t = min(min(t2, t3), t5);
-            dp[i] = t;
-            if (t2 == t) {
-                p2++;
-            }
-            if (t3 == t) {
-                p3++;
-            }
-            if (t5 == t) {
-                p5++;
-            }
-        }
-        return dp[n];
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int nthUglyNum(int n);
+
+int main() {
+    cout << nthUglyNum(4) << endl;
+    return 0;
+}
+
+int nthUglyNum(int n) {
+    vector<int> dp(n, 1);
+    int p2 = 0, p3 = 0, p5 = 0;
+    for (int i = i; i < n; i++) {
+        int n2 = dp[p2] * 2,n3 = dp[p3] * 3, n5 = dp[p5] * 5;
+        int next = min(min(n2, n3), n5);  
+        dp[i] = next;
+        if (n2 <= next) p2++;
+        if (n3 <= next) p3++;
+        if (n5 <= next) p5++;
     }
-};
+    return dp.back();
+}
+
+
