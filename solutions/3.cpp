5ca7c6c03f0251fe47cb289ed84482fd0ddef85f
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2025-09-14 12:21:56 +0800

    [fix]更新滑动窗口解法
    Change-Id: I3245c184c953108d4a3e0fe1997464bcd629e082

--- solutions/3.cpp
+++ solutions/3.cpp
@@ -1,13 +1,23 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> v(256, -1);
-        int start = 0, res = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (v[s[i]] >= start) start = v[s[i]] + 1;
-            v[s[i]] = i;
-            res = max(res, i - start + 1);
-        }
-        return res;
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int maxLenSubstring(string &s);
+
+int main() {
+    return 0;
+}
+
+int maxLenSubstring(string &s) {
+    int res = 0, start = 0;
+    vector<int> pos(256, -1);
+    for (int i = 0; i < s.size(); i++) {
+        if (pos[s[i]] >= start) {  // 修复：检查重复字符是否在当煎窗口内
+            start = pos[s[i]] + 1;  // 修复：移动到重复字符的下一个位置
+        } 
+        pos[s[i]] = i;
+        res = max(res, i - start + 1);
     }
-};
+    return res;
+}
