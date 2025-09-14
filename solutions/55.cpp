1f4ef691d075ebda0e28e3cd1d9d45ab2c040f0c
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2025-09-14 16:32:58 +0800

    [fix]更新跳跃游戏贪心解法
    Change-Id: Ia44fab9d5331afc9971ec53f5dc2d162e30ec112

--- solutions/55.cpp
+++ solutions/55.cpp
@@ -1,11 +1,25 @@
+/*
+ * @lc app=leetcode id=55 lang=cpp
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size(), max_reach = 0, i = 0;
-        while (i < n && i <= max_reach) {
-            max_reach = max(max_reach, i + nums[i]);
-            i++;
+        int maxReach = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = max(maxReach, i + nums[i]);
+            if (maxReach >= nums.size() - 1) {
+                return true;
+            }
         }
-        return i == n;
+        return false;
     }
 };
+// @lc code=end
+
