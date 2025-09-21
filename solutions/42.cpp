5f7615c685ff76ad13b57f109ea840c9551a26de
ZHAOMINGXING.93 <ZHAOMINGXING.93@bytedance.com>
2025-09-21 15:54:39 +0800

    [fix]使用 ai 重写雨水收集

--- solutions/42.cpp
+++ solutions/42.cpp
@@ -1,18 +1,35 @@
-class Solution {
-public:
-    int trap(vector<int>& height) {
-        stack<int> s;
-        int n = height.size(), res = 0, i = 0;
-        while (i < n) {
-            while (!s.empty() && height[i] > height[s.top()]) {
-                int bottom = height[s.top()]; s.pop();
-                if (!s.empty()) {
-                    int top = min(height[s.top()], height[i]), width = i - s.top() - 1;
-                    res += (top - bottom) * width;
-                }
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int maxWater(vector<int> &nums);
+
+int main() {
+    vector<int> nums = {0,1,0,2,1,0,1,3,2,1,2,1};
+    cout << maxWater(nums) << endl;
+    return 0;
+}
+
+
+int maxWater(vector<int> &nums) {
+    int res = 0;
+    stack<int> s;
+    for (int i = 0; i < nums.size(); i++) {
+        while (!s.empty() && nums[s.top()] < nums[i]) {
+            int buttom = s.top();
+            s.pop();
+            if (!s.empty()) {
+                int left = s.top(), right = i;
+                int width = right - left - 1;
+                int height = min(nums[left], nums[right]) - nums[buttom];
+                res += width * height;
             }
-            s.push(i++);
         }
-        return res;
+        s.push(i);
     }
-};
+    return res;
+}
