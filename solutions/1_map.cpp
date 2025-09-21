6196709bd3ac486c69a3b85f6e9d90d941ac2a80
ZHAOMINGXING.93 <ZHAOMINGXING.93@bytedance.com>
2025-09-21 14:06:55 +0800

    [fix]更新hash 表解法

--- solutions/1_map.cpp
+++ solutions/1_map.cpp
@@ -1,11 +1,37 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) m[target - nums[i]] = i;
-        for (int i = 0; i < nums.size(); i++) {
-            if (m.find(nums[i]) != m.end() && i != m[nums[i]]) return {i, m[nums[i]]};
-        }
-        return {0, 0};
+#include<iostream>
+#include <unordered_map>
+#include<vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target);
+
+int main() {
+    vector<int> nums = {1,2,0,-1,3,0};
+    auto res = twoSum(nums, 0);
+    if (res.size() == 2) {
+        cout << res[0] << "_" << res[1] << endl;
     }
-};
+    return 0;
+
+}
+
+
+
+vector<int> twoSum(vector<int> &nums, int target) {
+    vector<int> res;
+    if (nums.size() < 2) {
+        return res;
+    }
+    unordered_map<int, int> m;
+    for(int i = 0; i < nums.size(); i++) {
+        m[nums[i]] = i;
+    }
+    for (int i = 0; i < nums.size(); i++) {
+        if (m.find(target - nums[i]) != m.end() && i != m[target-nums[i]]) {
+            res = {i, m[target - nums[i]]};
+            break;
+        }
+    }
+    return res;
+}
