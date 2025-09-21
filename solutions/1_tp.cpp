0e418ebf067286d94a373d2a74ca9f403b9ab30a
ZHAOMINGXING.93 <ZHAOMINGXING.93@bytedance.com>
2025-09-21 11:29:11 +0800

    [fix]更新双指针解法

--- solutions/1_tp.cpp
+++ solutions/1_tp.cpp
@@ -1,28 +1,42 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> copy(nums);
-        sort(copy.begin(), copy.end());
-        int l = 0, r = nums.size() - 1, sum;
-        while (l < r) {
-            sum = copy[l] + copy[r];
-            if (sum == target) break;
-            else if (sum > target) r--;
-            else l++;
-        }
-        vector<int> res(2, 0);
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == copy[l]) {
-                res[0] = i; break;
-            }
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == copy[r]) {
-                if (copy[l] != copy[r] || (copy[l] == copy[r] && i != res[0])) {
-                    res[1] = i; break;
-                }
-            }
-        }
-        return res;
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> twoSum(const vector<int> &nums, int sum);
+
+
+int main() {
+    vector<int> v = {1,2,3,2,2};
+    auto res = twoSum(v, 4);
+    if (res.size() == 2) {
+        cout << res[0] << "_" << res[1] << endl;
     }
-};
+    return 0;
+
+}
+
+vector<int> twoSum(const vector<int> &nums, int sum) {
+    vector<int> res;
+    if (nums.size() < 2) {return res;}
+    vector<pair<int, int>> v;
+
+    v.reserve(nums.size());
+    for(int i = 0; i < nums.size(); i++) {
+        v.emplace_back(nums[i], i);
+    }
+    sort(v.begin(), v.end());
+    
+    int l = 0, r = v.size() - 1; 
+    while (l < r) {
+        int t = v[l].first + v[r].first;
+        if (t == sum) {
+            break;
+        }
+        if (t < sum) {l++;} else {r--;}
+    }
+    if (l < r) {
+        res = {v[l].second, v[r].second};
+    }
+    return res;
+}
